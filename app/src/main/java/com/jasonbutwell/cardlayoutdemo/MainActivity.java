ed58b17d47f261c2340c352d0674639e61c152ae
Jason Butwell <jasonbutwell@gmail.com>
2016-10-18 23:02:08 +0100

    Added layout XML and java code in OnCreate() to create an example card fragment and show it on the android wear device.

--- app/src/main/java/com/jasonbutwell/cardlayoutdemo/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/cardlayoutdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jasonbutwell.cardlayoutdemo;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.wearable.view.CardFragment;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
@@ -13,12 +16,35 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+
+                // For the card we need the fragment manager
+
+                FragmentManager fragmentManager = getFragmentManager();
+
+                // Set up the transaction
+
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                // Creation of the card fragment
+
+                CardFragment cardFragment = CardFragment.create(
+                        getString(R.string.cftitle),                        //  card title
+                        getString(R.string.cfdesc),                         //  card description
+                        android.R.drawable.ic_menu_add);                   //   card icon
+
+                // Add the layout and commit to the fragment
+
+                fragmentTransaction.add(R.id.frame_layout, cardFragment);
+                fragmentTransaction.commit();
             }
+
         });
     }
 }
